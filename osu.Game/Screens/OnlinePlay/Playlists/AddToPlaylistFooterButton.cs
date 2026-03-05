06297d8584d96be2e573489a6b53ff61eb1b7fc6
Krzysztof Gutkowski <krzysio.gutkowski@gmail.com>
2026-03-05 12:54:20 +0100

    Extract common logic from `AddToPlaylistFooterButton`
    Depends on #36802 and ppy/osu-resources#408.
    
    This moves out the common layout from `AddToPlaylistFooterButton` to
    a separate `ShearedFooterButton`, that is intended to be used
    in other screens which I'm currently working on migrating them to the
    new footer.
    
    It also adds a new 'add' icon to be used on the playlists button.
    
    Pushing this ahead of time (without any other buttons using it yet)
    mostly to get the new icon on the add to playlist button.
    
    I've set the icon size to `20`. Could get it smaller, howvever
    I think it should be a *bit* taller than the button label.
    
    # Conflicts:
    #	osu.Game/Screens/OnlinePlay/Playlists/AddToPlaylistFooterButton.cs

--- osu.Game/Screens/OnlinePlay/Playlists/AddToPlaylistFooterButton.cs
+++ osu.Game/Screens/OnlinePlay/Playlists/AddToPlaylistFooterButton.cs
@@ -2,16 +2,13 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using osu.Framework.Allocation;
-using osu.Framework.Graphics;
 using osu.Game.Graphics;
-using osu.Game.Graphics.Sprites;
-using osu.Game.Graphics.UserInterface;
 using osu.Game.Localisation;
 using osu.Game.Screens.Footer;
 
 namespace osu.Game.Screens.OnlinePlay.Playlists
 {
-    public partial class AddToPlaylistFooterButton : ShearedButton, IFooterButton
+    public partial class AddToPlaylistFooterButton : ShearedFooterButton, IFooterButton
     {
         [BackgroundDependencyLoader]
         private void load(OsuColour colours)
@@ -21,28 +18,8 @@ namespace osu.Game.Screens.OnlinePlay.Playlists
             DarkerColour = colours.Blue3;
             LighterColour = colours.Blue1;
 
-            ButtonContent.Children = new Drawable[]
-            {
-                new OsuSpriteText
-                {
-                    Anchor = Anchor.Centre,
-                    Origin = Anchor.Centre,
-                    X = -10,
-                    Font = OsuFont.TorusAlternate.With(size: 17),
-                    Text = OnlinePlayStrings.FooterButtonPlaylistAdd,
-                    UseFullGlyphHeight = false,
-                },
-                new OsuSpriteText
-                {
-                    Anchor = Anchor.CentreRight,
-                    Origin = Anchor.CentreRight,
-                    X = 35,
-                    Font = OsuFont.TorusAlternate.With(size: 20),
-                    Shadow = false,
-                    Text = "+",
-                    UseFullGlyphHeight = false,
-                },
-            };
+            Text = OnlinePlayStrings.FooterButtonPlaylistAdd;
+            Icon = OsuIcon.Add;
         }
     }
 }
